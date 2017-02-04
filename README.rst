99bf564e43883c1d6515ec9cda8b684fbb8e072c
Liam Schumm <liamschumm@icloud.com>
2017-02-04 13:20:19 -0600

    Initial code

---
+++ README.rst
@@ -0,0 +1,14 @@
+Vortex
+======
+
+Jump to directories.
+
+Credits
+=======
+
+This project was inspired by `@mfaerevaag`_ 's `wd`_.
+
+.. _@mfaerevaag: https://github.com/mfaerevaag
+
+.. _wd: https://github.com/mfaerevaag/wd
+
---
+++ weather.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# pylint's name standards are insane
+# pylint: disable=invalid-name
+
+# this file is imported from a different directory
+# pylint: disable=import-error
+
+# positional arguments are a good standard for commands
+# pylint: disable=unused-argument
+
+"""
+[weather.py]
+
+Defines the "weather" command.
+"""
+
+import urllib2
+
+verbs = {}
+
+def weather_for_city(city):
+    """Return the weather for a specific city."""
+    req = urllib2.Request("wttr.in/%s" % (x.strip().lower()))
+    res = urllib2.urlopen(req)
+    return res.read()
+    
+def weather(env, args, kwargs):
+    """[CITYNAME,...]@Return the weather (wttr.in) for all cities specified."""
+    return map(weather_for_city, args)
+
+verbs["weather"] = weather
